b6d85e5be65935a98f356b13bbbe670e00f74e67
Zhangxitian <1325734688@qq.com>
2023-11-07 19:58:41 +0800

    login

---
+++ src/api/login.ts
@@ -0,0 +1,27 @@
+import { http } from '@/utils/http'
+
+/**
+ * 小程序登录
+ * @param data 请求参数
+ */
+export const postLoginWxMinAPI = (data: LoginParams) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/user/login/wxMin',
+    data
+  })
+}
+
+/**
+ * 小程序登录_内测版
+ * @param phoneNumber 模拟手机号码
+ */
+export const postLoginWxMinSimpleAPI = (phoneNumber: string) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/user/login/wxMin/simple',
+    data: {
+      phoneNumber
+    }
+  })
+}
--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -11,6 +11,11 @@ const memberStore = useMemberStore()
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <uni-section title="设置" type="line">
+      <uni-list>
+        <uni-list-item title="去登录" show-arrow :to="`/pages/login/login`"> </uni-list-item>
+      </uni-list>
+    </uni-section>
   </view>
 </template>
 
